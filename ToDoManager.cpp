a24ed3cd700ec0d5f079f0ebfd5323a99ac877dd
MasterAdamant <ivan.vladislavovich.2007@gmail.com>
2024-12-10 13:26:38 +0200

    Init

---
+++ ToDoManager.cpp
@@ -0,0 +1,15 @@
+﻿#include "TaskLib.h"
+#include <chrono>
+
+using namespace std;
+
+int main() {
+	Task hometask("Homework", "Do math", chrono::system_clock::now(), chrono::system_clock::time_point(), 1);
+	Task project("Project", "Develop project", chrono::system_clock::time_point(), chrono::system_clock::time_point(), 3);
+	Task job("Work", "Don't sleep", chrono::system_clock::time_point(), chrono::system_clock::time_point(), 2);
+	TaskManager manager({hometask, project, job});
+
+	Organiser::SetManager(manager);
+	Organiser::Cycle();
+	return 0;
+}
